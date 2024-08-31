1565ab6d281880692c7dee07fc567eb37e41c9ec
aarushiksk <aarushee002@gmail.com>
2024-08-31 17:18:04 +0530

    Exception handling

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name="MLproject"
+project_name="mlproject"
 list_of_files=[
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
